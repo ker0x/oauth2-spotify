a1776e19a843130af218cc2a42bcb1e8aad4a48f
Romain Monteil <rmo@emakina.fr>
2020-03-03 10:03:21 +0100

    Update Authorization scopes, update CI configuration

--- src/Provider/Spotify.php
+++ src/Provider/Spotify.php
@@ -20,30 +20,43 @@ class Spotify extends AbstractProvider
     public const RESPONSE_TYPE = 'code';
 
     // Available scopes.
-    public const SCOPE_APP_REMOTE_CONTROL = 'app-remote-control';
-    public const SCOPE_PLAYLIST_MODIFY_PRIVATE = 'playlist-modify-private';
-    public const SCOPE_PLAYLIST_MODIFY_PUBLIC = 'playlist-modify-public';
-    public const SCOPE_PLAYLIST_READ_COLLABORATIVE = 'playlist-read-collaborative';
-    public const SCOPE_PLAYLIST_READ_PRIVATE = 'playlist-read-private';
-    public const SCOPE_STREAMING = 'streaming';
-    public const SCOPE_USER_READ_PRIVATE = 'user-read-private';
-    public const SCOPE_USER_READ_BIRTHDATE = 'user-read-birthdate';
-    public const SCOPE_USER_READ_EMAIL = 'user-read-email';
+
+    // Images
+    public const SCOPE_UGC_IMAGE_UPLOAD = 'ugc-image-upload';
+
+    // Spotify Connect
+    public const SCOPE_USER_MODIFY_PLAYBACK_STATE = 'user-modify-playback-state';
+    public const SCOPE_USER_READ_PLAYBACK_STATE = 'user-read-playback-state';
+    public const SCOPE_USER_READ_CURRENTLY_PLAYING = 'user-read-currently-playing';
+
+    // Listening History
     public const SCOPE_USER_TOP_READ = 'user-top-read';
     public const SCOPE_USER_READ_RECENTLY_PLAYED = 'user-read-recently-played';
+
+    // Library
     public const SCOPE_USER_LIBRARY_MODIFY = 'user-library-modify';
     public const SCOPE_USER_LIBRARY_READ = 'user-library-read';
-    public const SCOPE_USER_READ_CURRENTLY_PLAYING = 'user-read-currently-playing';
-    public const SCOPE_USER_READ_PLAYBACK_STATE = 'user-read-playback-state';
-    public const SCOPE_USER_MODIFY_PLAYBACK_STATE = 'user-modify-playback-state';
+
+    // Follow
     public const SCOPE_USER_FOLLOW_MODIFY = 'user-follow-modify';
     public const SCOPE_USER_FOLLOW_READ = 'user-follow-read';
 
+    // Playlist
+    public const SCOPE_PLAYLIST_READ_PRIVATE = 'playlist-read-private';
+    public const SCOPE_PLAYLIST_MODIFY_PUBLIC = 'playlist-modify-public';
+    public const SCOPE_PLAYLIST_MODIFY_PRIVATE = 'playlist-modify-private';
+    public const SCOPE_PLAYLIST_READ_COLLABORATIVE = 'playlist-read-collaborative';
+
+    // User
+    public const SCOPE_USER_READ_PRIVATE = 'user-read-private';
+    public const SCOPE_USER_READ_EMAIL = 'user-read-email';
+
+    // Playback
+    public const SCOPE_APP_REMOTE_CONTROL = 'app-remote-control';
+    public const SCOPE_STREAMING = 'streaming';
+
     /**
      * Spotify constructor.
-     *
-     * @param array $options
-     * @param array $collaborators
      */
     public function __construct(array $options = [], array $collaborators = [])
     {
@@ -56,8 +69,6 @@ class Spotify extends AbstractProvider
 
     /**
      * Returns the base URL for authorizing a client.
-     *
-     * @return string
      */
     public function getBaseAuthorizationUrl(): string
     {
@@ -66,10 +77,6 @@ class Spotify extends AbstractProvider
 
     /**
      * Returns the base URL for requesting an access token.
-     *
-     * @param array $params
-     *
-     * @return string
      */
     public function getBaseAccessTokenUrl(array $params): string
     {
@@ -78,10 +85,6 @@ class Spotify extends AbstractProvider
 
     /**
      * Returns the URL for requesting the resource owner's details.
-     *
-     * @param AccessToken $token
-     *
-     * @return string
      */
     public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
@@ -93,8 +96,6 @@ class Spotify extends AbstractProvider
      *
      * This should only be the scopes that are required to request the details
      * of the resource owner, rather than all the available scopes.
-     *
-     * @return array
      */
     protected function getDefaultScopes(): array
     {
@@ -104,8 +105,7 @@ class Spotify extends AbstractProvider
     /**
      * Checks a provider response for errors.
      *
-     * @param ResponseInterface $response
-     * @param array|string      $data     Parsed response data
+     * @param array|string $data Parsed response data
      *
      * @throws IdentityProviderException
      */
@@ -127,11 +127,6 @@ class Spotify extends AbstractProvider
     /**
      * Generates a resource owner object from a successful resource owner
      * details request.
-     *
-     * @param array       $response
-     * @param AccessToken $token
-     *
-     * @return ResourceOwnerInterface
      */
     protected function createResourceOwner(array $response, AccessToken $token): ResourceOwnerInterface
     {
